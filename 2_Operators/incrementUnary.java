b221b20eb2b5ff27cc168cc948f194dc77d6fb4c
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-12-29 23:36:26 +0530

    To understand how increment operators work.

--- 2_Operators/incrementUnary.java
+++ 2_Operators/incrementUnary.java
@@ -1,13 +1,13 @@
 public class IncrementUnary {
     public static void main(String args[]) {
-        /*  pre decreament "++a" will first change the value of its own variable...here "a" 
+        /*  pre increment "++a" will first change the value of its own variable...here "a" 
          and then assign it to the other variable */
         int a = 10;
         int b = ++a;
         System.out.println(a);
         System.out.println(b);
 
-        /* post decreament "a++" will first assign the value to the other variable 
+        /* post increment "a++" will first assign the value to the other variable 
         and then change it for its own variable...here "x"  */
         System.out.println("-------------------------------");
         int x = 20;
